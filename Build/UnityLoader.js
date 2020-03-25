1afc84a4db83c9b246910e540152b1dd64734055
zodiacsolutionssg <54932623+zodiacsolutionssg@users.noreply.github.com>
2020-03-25 14:40:40 +0700

    Add files via upload

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -2039,16 +2039,7 @@ var UnityLoader = UnityLoader || {
         }
     }(),
     compatibilityCheck: function(e, t, r) {
-        UnityLoader.SystemInfo.hasWebGL ? UnityLoader.SystemInfo.mobile ? e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.", [{
-            text: "OK",
-            callback: t
-        }]) : ["Edge", "Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1 ? e.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.", [{
-            text: "OK",
-            callback: t
-        }]) : t() : e.popup("Your browser does not support WebGL", [{
-            text: "OK",
-            callback: r
-        }])
+			t();
     },
     Blobs: {},
     loadCode: function(e, t, r) {
